93d06354b7a8b5909eb105c856a33996f8b1a41a
Xmitzi <miriam.belda@edu.uah.es>
2026-03-09 21:12:29 +0100

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './stage-patch-reader_(1)';
+import App from './stage-patch-reader_(2)';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
