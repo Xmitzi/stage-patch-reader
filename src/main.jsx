9dd937275dd2d85462168ae7fbc0e2d2bd0d340a
Xmitzi <miriam.belda@edu.uah.es>
2026-03-09 20:32:14 +0100

    Added index.html

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import StagePatchReader from './stage-patch-reader';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StagePatchReader />
+  </React.StrictMode>
+);
